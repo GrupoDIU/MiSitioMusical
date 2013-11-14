f4e5d78ad7916f3d2540c151b288e526aed2d6c2
omarcuba <OMAR@OMAR-PC>
2013-11-13 22:31:13 -0500

    Arreglando mis cambios

--- src/com/example/misitiomusical/Lista_entrada_albumes.java
+++ src/com/example/misitiomusical/Lista_entrada_albumes.java
@@ -1,11 +1,15 @@
 package com.example.misitiomusical;
 
 
-public class Lista_entrada_albumes {
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+public class Lista_entrada_albumes implements Parcelable{
 	private int idImagen; 
 	private String textoEncima; 
 	private String textoDebajo; 
-	  
+	private static String TAG = "** clase  Lista_entrada_albumes **";
 	public Lista_entrada_albumes (int idImagen, String textoEncima, String textoDebajo) { 
 	    this.idImagen = idImagen; 
 	    this.textoEncima = textoEncima; 
@@ -22,5 +26,20 @@ public class Lista_entrada_albumes {
 	
 	public int get_idImagen() {
 	    return idImagen; 
+	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		// TODO Auto-generated method stub
+		dest.writeString(textoEncima);
+		dest.writeString(textoDebajo);
+		dest.writeInt(idImagen);
+		Log.d(TAG,"ESCRIBIENDO A Parcel");
 	} 
 }
