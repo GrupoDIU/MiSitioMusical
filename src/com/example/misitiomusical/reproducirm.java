b7272769f54bfd448229eb9c6637d766ac14afd8
Asus <Asus@Asus-PC>
2013-11-23 20:18:57 -0500

    audio1

--- src/com/example/misitiomusical/reproducirm.java
+++ src/com/example/misitiomusical/reproducirm.java
@@ -1,10 +1,13 @@
 package com.example.misitiomusical;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
 
@@ -54,4 +57,32 @@ public class reproducirm extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void ejecutarAlex(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        Uri data = Uri.parse("file:///sdcard" + "/detras_de_un_cristal.mp3");
+        intent.setDataAndType(data, "audio/mp3");
+        startActivity(intent);
+    }
+	
+	public void ejecutarCamila(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        Uri data = Uri.parse("file:///sdcard" + "/entre_tus_alas.mp3");
+        intent.setDataAndType(data, "audio/mp3");
+        startActivity(intent);
+    }
+	
+	public void ejecutarLinking(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        Uri data = Uri.parse("file:///sdcard" + "/foreword.mp3");
+        intent.setDataAndType(data, "audio/mp3");
+        startActivity(intent);
+    }
+	
+	public void ejecutarReik(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+        Uri data = Uri.parse("file:///sdcard" + "/te_fuiste_de_aqui.mp3");
+        intent.setDataAndType(data, "audio/mp3");
+        startActivity(intent);
+    }
 }
